7bc5703bfa5ded3a76fa33828cc1b244006295ee
Josue Aquino <aquinojosue3@gmail.com>
2021-05-13 12:49:29 -0600

    incorporacion de los csv de gimnasios

--- grass_scripts/csv_import.py
+++ grass_scripts/csv_import.py
@@ -3,15 +3,14 @@ import json
 from grass.pygrass.vector import VectorTopo
 from grass.pygrass.vector.geometry import Point
 
-csv_path = '/home/jaquino/Documents/gimnasios.csv'
+csv_path = '/home/jaquino/sgg115/csv/gimnasios_sv.csv'
 # Primero que nada cargamos los datos, si no cargan pos no se sigue xd
 with open(csv_path, 'r') as data:
     # Creamos la definicion de columnas
     cols = [(u'cat',   'INTEGER PRIMARY KEY'),
             (u'nombre_gym',  'VARCHAR'),
-            (u'telefono_gym', 'VARCHAR'),
             (u'depto', 'VARCHAR'),
-            (u'muni', 'VARCHAR')]
+            (u'telefono_gym', 'VARCHAR')]
     # Iniciamos un nuevo mapa vectorial
     vector_puntos = VectorTopo('gimnasios_script')
     # Importante! si se pone true a overwrite sera para hacer una nueva tabla, pero false para agregar datos.
@@ -21,7 +20,7 @@ with open(csv_path, 'r') as data:
         point = Point(float(gimnasio['lon']),float(gimnasio['lat']))
         vector_puntos.write(
             point, 
-            (gimnasio['nombre_gym'],gimnasio['telefono_gym'],gimnasio['depto'],gimnasio['muni'],)
+            (gimnasio['nombre_gym'],gimnasio['depto'],gimnasio['telefono_gym'],)
         )
     # Guardamos los datos, sobretodo porque grass por defecto usa una base de datos sqlite
     vector_puntos.table.conn.commit()
